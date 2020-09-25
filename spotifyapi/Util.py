90c28f6cf42c7f88d7ecac4107c22626a983d792
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-09-25 14:20:53 +0530

    Update Util.py

--- spotifyapi/Util.py
+++ spotifyapi/Util.py
@@ -1,16 +1,13 @@
-class AttrDict():
-  def __init__(self, dictionary: dict):
-    for key in dictionary.keys():
-      setattr(self, key, dictionary[key])
-
+# Import Packages
 import base64
+import urllib
 
+# Text to base64
 def b64(text: str):
   text = text.encode('ascii')
   return base64.b64encode(text).decode('ascii')
 
-import urllib
-
+# Parsing text to uri component like in js
 def encodeURIComponent(text: str):
   text = text.encode('utf-8')
   return urllib.parse.quote(text)
