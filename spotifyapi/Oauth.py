a5370992af627fff72843152ab4e3f34b1100515
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-09-25 11:14:05 +0530

    Create Oauth.py

---
+++ spotifyapi/Oauth.py
@@ -0,0 +1,22 @@
+import requests
+from .Util import b64, AttrDict
+
+class Auth():
+
+  def __init__(self, oauth: str):
+    self.token = oauth
+
+  def get(self, client_id: str, client_secret: str):
+    header_data = b64(str(client_id) + ':' + str(client_secret))
+    header = {
+      'Authorization': 'Basic {}'.format(header_data)
+    }
+
+    return requests.request(
+      'POST',
+      'https://accounts.spotify.com/api/token',
+      data={
+        'grant_type': 'client_credentials'
+      },
+      headers=header
+    ).json()['access_token']
