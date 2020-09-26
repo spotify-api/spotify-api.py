405448a189e3b6490a88a5be1ecea60e170788bb
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-09-26 19:19:18 +0530

    Fixed styling

--- spotifyapi/Util.py
+++ spotifyapi/Util.py
@@ -4,10 +4,10 @@ import urllib
 
 # Text to base64
 def b64(text: str):
-  text = text.encode('ascii')
-  return base64.b64encode(text).decode('ascii')
+    text = text.encode('ascii')
+    return base64.b64encode(text).decode('ascii')
 
 # Parsing text to uri component like in js
 def encodeURIComponent(text: str):
-  text = text.encode('utf-8')
-  return urllib.parse.quote(text)
+    text = text.encode('utf-8')
+    return urllib.parse.quote(text)
