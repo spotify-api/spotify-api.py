049b51c74b1265672af244de26bc76d2ed9e9b59
Sfue <admin@starstracker.xyz>
2020-09-28 11:36:54 +0530

    update __init__.py no commenting

--- spotifyapi/__init__.py
+++ spotifyapi/__init__.py
@@ -12,17 +12,22 @@ from .Track import Track
 from .Artist import Artist
 from .User import User
 from .Album import Album
+from .Playlist import Playlist
 
 # Client Class
+
+
 class Client():
 
-    def __init__(self, token:str):
+    def __init__(self, token: str):
         self.token = token
         self.oauth = Auth(self.token)
         self.track = Track(self.token)
         self.artist = Artist(self.token)
         self.user = User(self.token)
         self.album = Album(self.token)
+        self.playlist = Playlist(self.token)
+
 
 # Version v0.0.2
 __version__ = '0.0.2'
